f923f29824abdca0fca9b591cbb4f0dd07df2326
andela-hms <humphrey.musonye@andela.com>
2017-10-09 14:41:20 +0300

    create booklist container

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
     return [
         {title:'Javascript: The good parts' },
-        {title: 'Harry Potter'}
-        {title: 'The Dark Tower'}
+        {title: 'Harry Potter'},
+        {title: 'The Dark Tower'},
         {title: 'Eloquent Ruby'}
     ]
 }
